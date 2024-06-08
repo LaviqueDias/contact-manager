6a1f6e675fe4beea3b96bf1409d2535d09c3d212
kevynlohan05 <kevynlohan05@gmail.com>
2024-06-08 16:36:40 -0300

    Enviando função de listar

--- contacts/contact_manager.py
+++ contacts/contact_manager.py
@@ -14,3 +14,13 @@ def adicionar_contato(nome, sobrenome, telefone, email, lista_contatos):
     
     lista_contatos.append(contato)
     return ja_adicionado
+
+def listar_contatos(lista):
+    lista_organizada = ""
+    for contato in lista:
+        lista_organizada += "Contato:\n"
+        for chave, valor in contato.items():
+            lista_organizada += f"{chave}: {valor}\n"
+        lista_organizada += "\n"
+
+    return lista_organizada
