2d198aa129144ecfc4dec8e83a339c99aeb71ee9
Isthanlley <isthanlleyfragoso99@gmail.com>
2024-06-08 17:49:47 -0300

    inserir a função de excluir

--- contacts/contact_manager.py
+++ contacts/contact_manager.py
@@ -24,3 +24,12 @@ def listar_contatos(lista):
         lista_organizada += "\n"
 
     return lista_organizada
+
+def excluir_contato(telefone, lista_contatos):
+        if telefone in telefone.contatos:
+            del lista_contatos.contatos[telefone]
+            print(f"Contato {telefone} excluído com sucesso.")
+        else:
+            print(f"Contato {telefone} não encontrado.")
+            
+        
