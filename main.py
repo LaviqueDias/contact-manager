778b5a436df67cc4e9da23d7346ca95ff2b6bfe1
LaviqueDias <lavique.oliveira@maisunifacisa.com.br>
2024-06-07 23:43:11 -0300

    atualização do main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from contacts_manager import *
+
+def main():
+    print('=====SISTEMA DE CONTATOS=====\n')
+    print("1. Adicionar contato")
+    print("2. Listar contato")
+    print("3. Remover contato")
+    print("4. Sair")
+
+    lista_contatos = []
+    opcao = 1
+    while opcao != 4:
+        opcao = int(input('Digite uma opção: '))
+
+        if opcao == 1:
+            pass
+        
+        elif opcao == 2:
+            pass
+
+        elif opcao == 3:
+            pass
+
+        elif opcao != 4:
+            print('Digite uma opção válida!')
+        
+
+
+if __name__ == '__main__':
+    main()
